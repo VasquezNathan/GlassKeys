3083546e101063dc0a27e9bbe60990d1eee0e97d
VasquezNathan <nathanvasquezdo1@gmail.com>
2022-01-30 21:59:14 -0800

    ready to start npm stuff

--- contracts/GlassKey.sol
+++ contracts/GlassKey.sol
@@ -3,28 +3,43 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GlassKey is ERC721URIStorage {
+contract GlassKey is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
     string private _customBaseURI;
+    uint256 private _price;
+    uint256 private _maxSupply;
 
     constructor(string memory customBaseURI_) ERC721("GlassKey", "KEY") {
         _customBaseURI = customBaseURI_;
+        _price = 0.05 ether;
+        _maxSupply = 256;
     }
 
-    function setBaseURI(string memory newBaseURI_) public {
+    function setBaseURI(string memory newBaseURI_) public onlyOwner {
         _customBaseURI = newBaseURI_;
     }
 
-    function purchase(uint256 quantity) public payable returns (uint256) {
-        _tokenIds.increment();
-
-        uint256 newItemId = _tokenIds.current();
-        _mint(msg.sender, newItemId);
-
-        return newItemId;
+    function purchase(uint256 quantity) public payable {
+        require(msg.value >= (_price* quantity), "Not enough ETH sent");
+        require(balanceOf(msg.sender) <= 10, "Cannot mint more than 10");
+        payable(owner()).transfer(msg.value);
+        for (uint i = 0; i < quantity; i++) {
+            mintForPurchase(msg.sender);
+        }
     }
+
+    function mintForPurchase(address buyer) private {
+        require (_tokenIds.current() <= _maxSupply, "256 keys already created");
+        _tokenIds.increment();
+        
+        uint256 newItemId = _tokenIds.current();
+        _mint(buyer, newItemId);
+    }
+
     function _baseURI() internal view virtual override returns (string memory) {
         return _customBaseURI;
     }
--- test/GlassKey.test.js
+++ test/GlassKey.test.js
@@ -1,23 +1,44 @@
-const TruffleAssert = require("truffle-assertions");
+const truffleAssert = require('truffle-assertions');
 const GlassKey = artifacts.require("./GlassKey.sol");
 
-contract("SimpleStorage", accounts => {
+contract("GlassKey", accounts => {
     it("allows a person to buy an NFT", async () => {
+        var contract_owner = accounts[0]
         let buyer = accounts[8];
         var glasskey = await GlassKey.deployed();
 
-        await glasskey.purchase(1, {
+        // eth blance of contract deployer before selling
+        var owner_starting_balance = await web3.eth.getBalance(contract_owner);
+        owner_starting_balance =  parseFloat(web3.utils.fromWei(owner_starting_balance, 'ether'));
+
+        // do the actual purchase
+        await glasskey.purchase(6, {
             from: buyer, 
-            value: web3.utils.toWei("0.05", "ether")
+            value: web3.utils.toWei("0.30", "ether")
         });
 
+        // eth balance of contract deployer after selling
+        var owner_ending_balance = await web3.eth.getBalance(contract_owner);
+        owner_ending_balance = parseFloat(web3.utils.fromWei(owner_ending_balance, 'ether')); 
+
+        // ensure the balance increases
+        assert.isAbove(owner_ending_balance, owner_starting_balance);
+
+        // display the before and after balance
+        console.log("Starting balance: ", owner_starting_balance);
+        console.log("Ending balance: ", owner_ending_balance);
+
+        // ensure that the buyer is now the owner of the token (buyer buys first token so he owns token with id 1)
         var owner = await glasskey.ownerOf(1);
         assert.equal(owner, buyer);
         
+        // ensure the token URI is getting set properly
         var token_uri = await glasskey.tokenURI(1);
         assert.equal(token_uri, "https://TPF.com/glasskeys/1");
 
-
+        // get the token balance and make sure that 4 tokens were bought
+        var token_balance = parseInt(await glasskey.balanceOf(buyer));
+        assert.equal(token_balance, 6);
     
     });
 
@@ -41,7 +62,10 @@ contract("SimpleStorage", accounts => {
     it("does not allow someone who is not the owner to change the URI", async () => {
         var glasskey = await GlassKey.deployed();
         
-        TruffleAssert.reverts(glasskey.setBaseURI("https://attack-uri.com"));
+        // makes sure it fails if the sender is not the owner of the contract
+        await truffleAssert.reverts(glasskey.setBaseURI("https://attack-uri.com",{
+            from: accounts[1]
+        }));
     });
 
 });
