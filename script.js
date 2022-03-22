4bcb98ccc305f06f8fcddca53f8266448265ad89
Nathan Vasquez <nathanvasquezdo1@gmail.com>
2022-03-21 20:02:53 -0700

    index

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ import Web3 from "web3";
 
 // "Web3.givenProvider" will be set if in an Ethereum supported browser.
 var web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
+const ethereumButton = document.querySelector('.enableEthereumButton');
+
+ethereumButton.addEventListener('click', () => {
+  //Will Start the metamask extension
+  web3.ethereum.request({ method: 'eth_requestAccounts' });
+});
