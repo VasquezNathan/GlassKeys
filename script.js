9f6589c51360ff24a8b9be00d2202e89aba2a486
Nathan Vasquez <nathanvasquezdo1@gmail.com>
2022-03-30 10:16:27 -0700

    Moving scripts to separate file.

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-// import Web3 from "https://cdnjs.cloudflare.com/ajax/libs/web3/1.7.1/web3.min.js";
-
-// // "Web3.givenProvider" will be set if in an Ethereum supported browser.
-// var web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
-// const ethereumButton = document.querySelector('.enableEthereumButton');
-
-// ethereumButton.addEventListener('click', () => {
-//   //Will Start the metamask extension
-//   web3.ethereum.request({ method: 'eth_requestAccounts' });
-// });
+var balanceOfDoa
+        // function sets/formats innerText of element with ticker id 
+        // declared as a function so it can be called after web3 promise.
+        var SetDoaBalance = function(balance) {
+            var tickerText = "DOA Balance: " + balance + " ETH";
+            document.getElementById("ticker").innerText = tickerText;
+        }
+        
+        // attempt to do web3 stuff.
+        try{
+            // retrieve web3 from the a provider.
+            // if there is no injected web3 from the browser (MetaMask or similar) then try infura remote node 
+            var web3 = new Web3(Web3.givenProvider || window.ethereum || "https://mainnet.infura.io/v3/8c8df2a012ff47e99a72135c14177be2");
+            web3.eth.getBalance("0x9c555c1dE936feDDe75a57c3f07ffeb984FC3EBd").then(function(result){
+                balanceOfDoa = web3.utils.fromWei(result);
+                SetDoaBalance(balanceOfDoa);
+            });
+        }
+        catch(error){
+            console.error(error);
+            document.getElementById("ticker").innerText = "If you are seeing this then MetaMask (or other hotwallet) is either not installed or is outdated.";
+        }
