67f914b0ccf6f04fbe3efe5f252c334486a1d0ab
VasquezNathan <nathanvasquezdo1@gmail.com>
2022-04-04 16:51:57 -0700

    Push.

--- small.js
+++ small.js
@@ -1,17 +1,17 @@
 const ethereumButton = document.querySelector('.ethButton');
+let accounts = [];
+
+var sendMoney = function() {
+    if (typeof window.ethereum !== 'undefined') {
+        window.ethereum.request({ method: 'eth_requestAccounts' }).then(function(result) {
+            console.log(result);
+        });
+       
+    }
+    alert("Please use a browser with MetaMask installed!");
+    
+}
 
 ethereumButton.addEventListener('click', () => {
     sendMoney();
 });
-
-var sendMoney = function() {
-    try {
-        var web3 = new Web3(Web3.givenProvider || window.ethereum || "https://mainnet.infura.io/v3/8c8df2a012ff47e99a72135c14177be2");
-        console.log(window.ethereum);
-        web3.eth.personal.getAccounts()
-            .then(console.log)
-    }
-    catch(error){
-        console.error(error);
-    }
-}
