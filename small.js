5cf15c4fe24bc0e019921c10f78d02a153772818
VasquezNathan <nathanvasquezdo1@gmail.com>
2022-04-04 16:34:44 -0700

    Testing.

--- small.js
+++ small.js
@@ -7,6 +7,7 @@ ethereumButton.addEventListener('click', () => {
 var sendMoney = function() {
     try {
         var web3 = new Web3(Web3.givenProvider || window.ethereum || "https://mainnet.infura.io/v3/8c8df2a012ff47e99a72135c14177be2");
+        console.log(window.ethereum);
         web3.eth.personal.getAccounts()
             .then(console.log)
     }
