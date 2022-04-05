a9f4457caaf7a26dd735a30f6ba6a30722c5e309
VasquezNathan <nathanvasquezdo1@gmail.com>
2022-04-04 17:05:55 -0700

    P.

--- small.js
+++ small.js
@@ -11,8 +11,6 @@ var sendMoney = function() {
                     from: accounts[0],
                     to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
                     value: '0x3782DACE9D90000',
-                    gasPrice: '0x09184e72a000',
-                    gas: '0x2710', 
                     },
                 ],
             })
