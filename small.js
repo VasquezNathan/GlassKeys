0ea90c37ff90367719669bdbce97ea8ecf46f6bc
VasquezNathan <nathanvasquezdo1@gmail.com>
2022-04-04 17:02:50 -0700

    P.

--- small.js
+++ small.js
@@ -4,22 +4,21 @@ let accounts = [];
 var sendMoney = function() {
     if (typeof window.ethereum !== 'undefined') {
         window.ethereum.request({ method: 'eth_requestAccounts' }).then(function(result) {
-            console.log(result);
+            accounts = result;
+            ethereum.request({ method: 'eth_sendTransaction',
+                params: [
+                    {
+                    from: accounts[0],
+                    to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
+                    value: '0x3782DACE9D90000',
+                    gasPrice: '0x09184e72a000',
+                    gas: '0x2710', 
+                    },
+                ],
+            })
+            .then((txHash) => console.log(txHash))
+            .catch((error) => console.error);
         });
-       
-        ethereum.request({ method: 'eth_sendTransaction',
-            params: [
-                {
-                from: accounts[0],
-                to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970',
-                value: '0x3782DACE9D90000',
-                gasPrice: '0x09184e72a000',
-                gas: '0x2710', 
-                },
-            ],
-        })
-        .then((txHash) => console.log(txHash))
-        .catch((error) => console.error);
     }
     else{
         alert("Please use a browser with MetaMask installed!");
